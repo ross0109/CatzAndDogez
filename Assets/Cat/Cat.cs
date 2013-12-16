286ac1f0eb7f74cd71d54b2ed56476becca50892
ross0109 <ross0109@gmail.com>
2013-12-16 13:30:00 -0500

    Everything else

---
+++ Assets/Cat/Cat.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cat : MonoBehaviour {
+	public Vector2 newPos = new Vector2 (0f, 0f);
+	public float gravity = -1f;
+	public float speed = 5f;
+	public float jumpHeight = .3f;
+	public int count = 0;
+	public float terminalVelocity = -5f;
+	private CharacterController controller;
+	
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+	
+	void Update () {
+		newPos.x = Input.GetAxis ("Horizontal1")*Time.deltaTime*speed;
+		if(Input.GetButtonDown("Jump1")){
+			if(controller.isGrounded || count == 1){
+				newPos.y = jumpHeight;
+				print ("Jumping");
+			}
+			count++;
+		}
+		if(count > 1 && controller.isGrounded){
+			count = 0;
+		}
+		newPos.y += gravity*Time.deltaTime;
+		controller.Move (newPos);
+	}
+}
---
+++ Assets/Doge/Doge.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Doge : MonoBehaviour {
+	public Vector2 newPos = new Vector2 (0f, 0f);
+	public float gravity = -1f;
+	public float speed = 4f;
+	public float jumpHeight = .25f;
+	private CharacterController controller;
+	
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+	
+	void Update () {
+		newPos.x = Input.GetAxis ("Horizontal2")*Time.deltaTime*speed;
+		if(Input.GetButtonDown("Jump2")){
+			if(controller.isGrounded){
+				newPos.y = jumpHeight;
+				print ("Jumping");
+			}
+		}
+		newPos.y += gravity*Time.deltaTime;
+		controller.Move (newPos);
+	}
+}
