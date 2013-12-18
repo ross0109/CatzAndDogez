ab8fbba9fd1a0cd9fd2fdcfab6a63c99f7f1bedf
AnthonyCopeland21 <Anthony.Copeland21@gmail.com>
2013-12-18 10:30:14 -0500

    New Script for Doge

--- Assets/Doge/Doge.cs
+++ Assets/Doge/Doge.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Doge : MonoBehaviour {
 	public Vector2 newPos = new Vector2 (0f, 0f);
-	public float jumpSpeed = 1f;
+	public float gravity = 1f;
 	public float speed = 3f;
 	public float jumpHeight = .25f;
 	private CharacterController controller;
@@ -14,27 +14,14 @@ public class Doge : MonoBehaviour {
 	
 	void Update () {
 		newPos.x = Input.GetAxis ("Horizontal2")*Time.deltaTime*speed;
-
 		if(Input.GetButtonDown("Jump2")){
-			Jump();
-		}
-		/*if(Input.GetButtonDown("Jump2")){
 			if(controller.isGrounded){
 				newPos.y = jumpHeight;
 				print ("Jumping");
 			}
-			if(){
+		}
 
-			}
-		}*/
-		transform.Translate(newPos.x);
 		newPos.y += gravity*Time.deltaTime;
 		controller.Move (newPos);
-	}
-
-	void Jump(){
-		if(controller.isGrounded){
-			rigidbody.AddForce(Vector2.up * jumpSpeed);
-		}
-	}
+	}	
 }
