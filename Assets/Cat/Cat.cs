91470ce5182714523e4173a36d53a4dcb3f66e24
AnthonyCopeland21 <Anthony.Copeland21@gmail.com>
2013-12-19 10:19:31 -0500

    Script changes

--- Assets/Cat/Cat.cs
+++ Assets/Cat/Cat.cs
@@ -4,10 +4,10 @@ using System.Collections;
 public class Cat : MonoBehaviour {
 	public Vector2 newPos = new Vector2 (0f, 0f);
 	private float gravity = -1f;
-	private float speed = 4f;
-	private float jumpHeight = .3f;
+	private float speed = 15f;
+	private float jumpHeight = .4f;
 	//public int count = 0;
-	private float terminalVelocity = -5f;
+	private float terminalVelocity = -.5f;
 	public CharacterController controller;
 	
 	void Start () {
@@ -26,7 +26,10 @@ public class Cat : MonoBehaviour {
 		/*if(count > 1 && controller.isGrounded){
 			count = 0;
 		}*/
-		newPos.y += gravity*Time.deltaTime;
+		if(newPos.y > terminalVelocity){
+			newPos.y += gravity*Time.deltaTime;
+		}
+
 		controller.Move (newPos);
 	}
 }
--- Assets/Doge/Doge.cs
+++ Assets/Doge/Doge.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class Doge : MonoBehaviour {
 	public Vector2 newPos = new Vector2 (0f, 0f);
-	private float gravity = 1f;
-	private float speed = 3f;
-	private float jumpHeight = .25f;
+	private float gravity = -1f;
+	private float speed = 15f;
+	private float jumpHeight = .4f;
 	public CharacterController controller;
-	private float height;
-	private float checkHeight;
-	private float maxGravity = -.25f;
+	//private float height;
+	//private float checkHeight;
+	private float maxGravity = -.5f;
 	
 	void Start () {
 		controller = GetComponent<CharacterController>();
@@ -24,11 +24,11 @@ public class Doge : MonoBehaviour {
 			}
 		}
 
-		height = transform.localPosition.y;
+		/*height = transform.localPosition.y;
 		if(newPos.y == height && !controller.isGrounded){
 			newPos.y = 0f;
 		}
-		checkHeight = height;
+		checkHeight = height;*/
 		if(newPos.y > maxGravity){
 			newPos.y += gravity * Time.deltaTime;
 		}
