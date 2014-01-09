6befc7bfa0a97d43c3a0c5305763c4b34192b523
ross0109 <ross0109@gmail.com>
2014-01-09 13:23:11 -0500

    yastuff

---
+++ Assets/TitleScreenStuff/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		if(Input.GetButtonDown("Return")){
+			Application.LoadLevel(1);
+		}
+	}
+}
