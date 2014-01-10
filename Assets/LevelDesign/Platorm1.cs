770004ae237300e6180bdeed422b580765948eb9
ross0109 <ross0109@gmail.com>
2014-01-10 12:28:20 -0500

    platform

---
+++ Assets/LevelDesign/Platorm1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platorm1 : MovePlatform {
+
+	void Start () {
+		controller = GetComponent<CharacterController>();
+		setDisplacement(.05f);
+		setSpeed(.01f);
+	}
+
+	void Update () {
+		movement();
+	}
+}
