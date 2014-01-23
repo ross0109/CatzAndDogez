36b81ed72e55683cb99d5720a820175d474e1540
ross0109 <ross0109@gmail.com>
2014-01-23 12:23:12 -0500

    sprite stuff

--- Assets/GameManager/CameraControl.cs
+++ Assets/GameManager/CameraControl.cs
@@ -2,14 +2,13 @@
 using System.Collections;
 
 public class CameraControl : MonoBehaviour {
-	
 
-	void Start () {
-		//Camera.current.
-	}
-	
+	public Transform Doge;
+	public Transform Cat;
+	public Vector3 center;
 
 	void Update () {
-
+		center = ((Doge.localPosition.x - Cat.localPosition.x)/2.0f) + Cat.localPosition.x;
+		transform.LookAt(center);
 	}
 }
